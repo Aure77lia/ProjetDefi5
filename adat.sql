771765318de079baddcd224a88ba91408219f52d
Nicolas LONGHI <nico784474@gmail.com>
2023-11-26 12:44:51 +0100

    BDD presenté

--- adat.sql
+++ adat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 25 nov. 2023 à 00:53
+-- Généré le : dim. 26 nov. 2023 à 11:43
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -425,6 +425,25 @@ INSERT INTO `a1` (`CTCN`, `CTCNIA`, `LTCN`, `TXCNT0`, `TXCNA0`, `TXCNU0`) VALUES
 (403, 0, 'LE VIEUX CERIER', 2334000, 481000, 2215000),
 (404, 0, 'VIEUX-RUFFEC', 1874000, 481000, 2215000),
 (416, 0, 'VITRAC-SAINT-VINCENT', 1957000, 481000, 2215000);
+
+-- --------------------------------------------------------
+
+--
+-- Doublure de structure pour la vue `baselocative`
+-- (Voir ci-dessous la vue réelle)
+--
+DROP VIEW IF EXISTS `baselocative`;
+CREATE TABLE IF NOT EXISTS `baselocative` (
+`CTCN` int(11)
+,`CTCNIA` int(11)
+,`NCCO` int(11)
+,`VBETF0` int(11)
+,`MOBS20` int(11)
+,`CNEX01` varchar(3)
+,`CNEX02` varchar(3)
+,`MNTPXC` int(11)
+,`MNBSMA` int(11)
+);
 
 -- --------------------------------------------------------
 
@@ -28077,6 +28096,113 @@ INSERT INTO `c1` (`CTCN`, `CTCNIA`, `NCCO`, `CNAC3`, `MNTPCT`, `MNBSMA`) VALUES
 (416, 0, 27333, '86901', 297, 1127),
 (416, 0, 27334, '6630Z', 0, 542),
 (416, 0, 27335, '9609Z', 0, 542);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nace`
+--
+
+DROP TABLE IF EXISTS `nace`;
+CREATE TABLE IF NOT EXISTS `nace` (
+  `Code` int(11) NOT NULL,
+  `libelleNAF` varchar(150) NOT NULL,
+  PRIMARY KEY (`Code`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `nace`
+--
+
+INSERT INTO `nace` (`Code`, `libelleNAF`) VALUES
+(1, 'Culture et production animale, chasse et services annexes'),
+(2, 'Sylviculture et exploitation forestière'),
+(3, 'Pêche et aquaculture'),
+(5, 'Extraction de houille et de lignite'),
+(6, 'Extraction d\'hydrocarbures'),
+(7, 'Extraction de minerais métalliques'),
+(8, 'Autres industries extractives'),
+(9, 'Services de soutien aux industries extractives'),
+(10, 'Industries alimentaires'),
+(11, 'Fabrication de boissons'),
+(12, 'Fabrication de produits à base de tabac'),
+(13, 'Fabrication de textiles'),
+(14, 'Industrie de l\'habillement'),
+(15, 'Industrie du cuir et de la chaussure'),
+(16, 'Travail du bois et fabrication d\'articles en bois et en liège, à l’exception des meubles ; fabrication d’articles en vannerie et sparterie'),
+(17, 'Industrie du papier et du carton'),
+(18, 'Imprimerie et reproduction d\'enregistrements'),
+(19, 'Cokéfaction et raffinage'),
+(20, 'Industrie chimique'),
+(21, 'Industrie pharmaceutique'),
+(22, 'Fabrication de produits en caoutchouc et en plastique'),
+(23, 'Fabrication d\'autres produits minéraux non métalliques'),
+(24, 'Métallurgie'),
+(25, 'Fabrication de produits métalliques, à l’exception des machines et des équipements'),
+(26, 'Fabrication de produits informatiques, électroniques et optiques'),
+(27, 'Fabrication d\'équipements électriques'),
+(28, 'Fabrication de machines et équipements nca'),
+(29, 'Industrie automobile'),
+(30, 'Fabrication d\'autres matériels de transport'),
+(31, 'Fabrication de meubles'),
+(32, 'Autres industries manufacturières'),
+(33, 'Réparation et installation de machines et d\'équipements'),
+(35, 'Production et distribution d\'électricité, de gaz, de vapeur et d\'air conditionné'),
+(36, 'Captage, traitement et distribution d\'eau'),
+(37, 'Collecte et traitement des eaux usées'),
+(38, 'Collecte, traitement et élimination des déchets ; récupération'),
+(39, 'Dépollution et autres services de gestion des déchets'),
+(41, 'Construction de bâtiments'),
+(42, 'Génie civil'),
+(43, 'Travaux de construction spécialisés'),
+(45, 'Commerce et réparation d\'automobiles et de motocycles'),
+(46, 'Commerce de gros, à l’exception des automobiles et des motocycles'),
+(47, 'Commerce de détail, à l’exception des automobiles et des motocycles'),
+(49, 'Transports terrestres et transport par conduites'),
+(50, 'Transports par eau'),
+(51, 'Transports aériens'),
+(52, 'Entreposage et services auxiliaires des transports'),
+(53, 'Activités de poste et de courrier'),
+(55, 'Hébergement'),
+(56, 'Restauration'),
+(58, 'Édition'),
+(59, 'Production de films cinématographiques, de vidéo et de programmes de télévision ; enregistrement sonore et édition musicale'),
+(60, 'Programmation et diffusion'),
+(61, 'Télécommunications'),
+(62, 'Programmation, conseil et autres activités informatiques'),
+(63, 'Services d\'information'),
+(64, 'Activités des services financiers, hors assurance et caisses de retraite'),
+(65, 'Assurance'),
+(66, 'Activités auxiliaires de services financiers et d\'assurance'),
+(68, 'Activités immobilières'),
+(69, 'Activités juridiques et comptables'),
+(70, 'Activités des sièges sociaux ; conseil de gestion'),
+(71, 'Activités d\'architecture et d\'ingénierie ; activités de contrôle et analyses techniques'),
+(72, 'Recherche-développement scientifique'),
+(73, 'Publicité et études de marché'),
+(74, 'Autres activités spécialisées, scientifiques et techniques'),
+(75, 'Activités vétérinaires'),
+(77, 'Activités de location et location-bail'),
+(78, 'Activités liées à l\'emploi'),
+(79, 'Activités des agences de voyage, voyagistes, services de réservation et activités connexes'),
+(80, 'Enquêtes et sécurité'),
+(81, 'Services relatifs aux bâtiments et aménagement paysager'),
+(82, 'Activités administratives et autres activités de soutien aux entreprises'),
+(84, 'Administration publique et défense ; sécurité sociale obligatoire'),
+(85, 'Enseignement'),
+(86, 'Activités pour la santé humaine'),
+(87, 'Hébergement médico-social et social'),
+(88, 'Action sociale sans hébergement'),
+(90, 'Activités créatives, artistiques et de spectacle'),
+(91, 'Bibliothèques, archives, musées et autres activités culturelles'),
+(92, 'Organisation de jeux de hasard et d\'argent'),
+(93, 'Activités sportives, récréatives et de loisirs'),
+(94, 'Activités des organisations associatives'),
+(95, 'Réparation d\'ordinateurs et de biens personnels et domestiques'),
+(96, 'Autres services personnels'),
+(97, 'Activités des ménages en tant qu\'employeurs de personnel domestique'),
+(98, 'Activités indifférenciées des ménages en tant que producteurs de biens et services pour usage propre'),
+(99, 'Activités des organisations et organismes extraterritoriaux');
 
 -- --------------------------------------------------------
 
@@ -83384,6 +83510,16 @@ INSERT INTO `p2` (`CTCN`, `CTCNIA`, `NCCO`, `MOTPC9`, `MNTPFG`, `MNCHFI`, `MNCHF
 (416, 0, 27333, b'0', 8, 0, 0),
 (416, 0, 27334, b'0', 0, 0, 0),
 (416, 0, 27335, b'0', 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la vue `baselocative`
+--
+DROP TABLE IF EXISTS `baselocative`;
+
+DROP VIEW IF EXISTS `baselocative`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `baselocative`  AS SELECT `c1`.`CTCN` AS `CTCN`, `c1`.`CTCNIA` AS `CTCNIA`, `c1`.`NCCO` AS `NCCO`, `p1`.`VBETF0` AS `VBETF0`, `p1`.`MOBS20` AS `MOBS20`, `p1`.`CNEX01` AS `CNEX01`, `p1`.`CNEX02` AS `CNEX02`, `p1`.`MNTPXC` AS `MNTPXC`, `c1`.`MNBSMA` AS `MNBSMA` FROM (`c1` join `p1`) WHERE ((`c1`.`CTCN` = `p1`.`CTCN`) AND (`c1`.`CTCNIA` = `p1`.`CTCNIA`) AND (`c1`.`NCCO` = `p1`.`NCCO`)) ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
